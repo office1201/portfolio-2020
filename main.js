a6ea8567a44021f435a3be4addb9c794ca2bb2f4
office0604 <73454756+office0604@users.noreply.github.com>
2020-12-18 12:05:20 +0900

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -11,3 +11,17 @@ document.addEventListener('scroll' , () => {
         navbar.classList.remove('navbar--dark');
     }
 });
+
+// Handle scrolling when tapping on the navbar menu
+// navbar menu를 눌렀을 때 화면이동을 위한 로직
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link == null) {
+        return;
+    }
+
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+});
